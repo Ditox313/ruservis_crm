e0aa6de9ac4933d77ac443ed348ad19b94651101
Xsodex <flex513513@gmail.com>
2025-02-18 14:43:39 +0300

    Установил корс и морган

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const authRoutes = require('./routes/auth.js');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 // const mongoose = require('mongoose');
 // const keys = require('./config/keys.js');
 // const passport = require('passport');
@@ -23,35 +23,29 @@ const app = express();
 
 
 
-
 // Регистрируем модуль bodyParser
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 // Регистрируем Morgan 
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
 // Регистрируем Cors
-// app.use(cors());
+app.use(cors());
+
 
 // Инициализируем passport и подключаем файл обработчик для логики защиты и проверки роутов
 // app.use(passport.initialize());
 // require('./middleware/passport')(passport);
 
 
-
-
 // Регистрируем роут auth
 app.use('/api/auth', authRoutes);
 
 
-
-
 // Добавляем возможность отдавать с сервера картинки по запросу. (Когда будет запрос к uploads, делай эту папку статической)
 // app.use('/uploads/cars', express.static('uploads/cars'));
-
-
 
 
 // Экспортируем наружу
